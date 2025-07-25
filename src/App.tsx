a99c15b3cee3fd28fbe0de1a9e48e9378fb492e7
TeckfortuneDev <sirishapullannagari1212@gmail.com>
2025-07-25 01:56:34 +0000

    fix: remove BrowserRouter basename for custom domain deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/nexus-transform">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
